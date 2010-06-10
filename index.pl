82f43ff5cf08231c03bc4e3f3a42cdf3d4fc145b
haro <haro@haro-s02.(none)>
2010-06-11 04:30:37 +0900

    ULを非同期通信で通信で行うための下地を作った

--- index.pl
+++ index.pl
@@ -22,7 +22,7 @@ my %vars = ('SiteName'=>'keiyac.org','SiteDescription'=>'i\'m hackin\' it');
 print "Content-Type: text/html; charset=UTF-8\n\n";
 my $htmlhead = "<meta charset=utf-8 /><link href=\"./css/kspade.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen,print\">";
 
-my ($htmlbdhd, $htmlbody, $sidebar, $htmlfoot);
+my ($htmlbdhd, $htmlbody, $sidebar, $htmlfoot) = ( '', '', '', '');
 
 # process cgi args
 my %query = &cgidec::getline($ENV{'QUERY_STRING'});
@@ -35,27 +35,25 @@ $vars{'NoSpacePageName'} =~ tr/ /+/;
 my $database = "./dat/kspade.db";
 my $dbargs = {PrintError=>1};
 my $data_source = "dbi:SQLite:dbname=$database","","",$dbargs;
-print $data_source;
 
 if ((defined $query{'init'} and $query{'init'} eq 'yes') and (&sql::tableexists($data_source) == 0)) {
-print "init mode";
 # database initialize (create the table)
 	&sql::create_table($data_source);
 	my $modified_date = &date::spridate('%04d/%02d/%02d %02d:%02d:%02d');
 	my $created_date = $modified_date;
 	my $body = &tmpl2html('html/tutorial.txt',\%vars);
 	&sql::do("insert into pages (title,lastmodified_date,created_date,tags,autotags,copyright,body)
-	          values ('TopPage','$modified_date','$created_date','Help','Help','Copyleft','$body');"
-	          ,$data_source);
+		values ('TopPage','$modified_date','$created_date','Help','Help','Copyleft','$body');"
+		,$data_source);
 	$htmlhead .= '<title>Miracle! Table was created!</title>';
 	$htmlbody .= '<p>Table was created. Please reload.</p>';
 }
 
 $htmlbdhd .= &tmpl2html('html/bodyhead.html',\%vars);
 $vars{'SidebarCategoryList'} = &listcategory("select tags from pages;"
-                                 ,"<dd><a href=\"./index.pl?cmd=category&amp;query=%s\">%s</a></dd>");
+	,"<dd><a href=\"./index.pl?cmd=category&amp;query=%s\">%s</a></dd>");
 $vars{'SidebarPagesList'} = &listpages("select title from pages order by lastmodified_date desc, title limit 5;"
-                           ,"<dd><a href=\"./index.pl?page=%s\">%s</a></dd>");
+	,"<dd><a href=\"./index.pl?page=%s\">%s</a></dd>");
 $sidebar  = &tmpl2html('html/sidebar.html',\%vars);
 
 
@@ -94,7 +92,7 @@ sub page {
 		$confer .= "<a href=\"files/$elements[0]\">$elements[1]</a> ";
 		$confer =~ s/[\[\]]+//g;
 	}
-	
+
 	my $filenum = @filedatas;
 	$htmlbody .= '</section><section><h2>Attached File</h2>'.$confer.'</section>' if $filenum == 1;
 	$htmlbody .= '</section><section><h2>Attached Files</h2>'.$confer.'</section>' if $filenum > 1;
@@ -119,8 +117,8 @@ sub post {
 
 	my ($title,$modifieddate,$tags,$autotags,$copyright,$body) = (&fetch2edit)[0,1,3,4,6,7];
 	&sql::do("update pages set title='$title', lastmodified_date='$modifieddate', tags='$tags',
-	          autotags='$autotags', copyright='$copyright', body='$body' where title='".$vars{'PageName'}."';"
-	          ,$data_source);
+		autotags='$autotags', copyright='$copyright', body='$body' where title='".$vars{'PageName'}."';"
+		,$data_source);
 
 } 
 sub new {
@@ -134,8 +132,8 @@ sub newpost {
 	my ($title,$created_date,$tags,$autotags,$copyright,$body) = (&fetch2edit)[0,2,3,4,6,7];	
 	$title = 'undefined'.rand(16384) if $title eq '';
 	&sql::do("insert into pages (title,lastmodified_date,created_date,tags,autotags,copyright,body)
-	          values ('$title','$created_date','$created_date','$tags','$autotags','$copyright','$body');"
-	          ,$data_source);
+		values ('$title','$created_date','$created_date','$tags','$autotags','$copyright','$body');"
+		,$data_source);
 
 }
 sub del {
@@ -149,25 +147,25 @@ sub delpage {
 # delete page
 	read (STDIN, my $postdata, $ENV{'CONTENT_LENGTH'});
 	#my %form = &cgidec::getline($postdata);
-		&sql::do("delete from pages where title='".$vars{'PageName'}."'",$data_source);
+	&sql::do("delete from pages where title='".$vars{'PageName'}."'",$data_source);
 
 }
 sub search {
 	my $query = &security::textalize(&security::exorcism($query{'query'}));
 	$query =~ s/\s/AND/g;
-		$vars{'Query'} = $query{'query'};
+	$vars{'Query'} = $query{'query'};
 	if (defined $query{'query'}) {
-	# normal search
+		# normal search
 		$vars{'PagesList'} = &listpages("select title from pages where body like '%$query%';"
-	                                        ,"<a href=\"./index.pl?page=%s\">%s</a><br />") if defined $query{'query'};
+			,"<a href=\"./index.pl?page=%s\">%s</a><br />") if defined $query{'query'};
 		$htmlhead .= '<title>Search &gt; Body@'.$vars{'SiteName'}.'</title>';
 		$htmlbody .= &tmpl2html('html/search.html',\%vars);
 		delete $vars{'PagesList'};
 
 	} else {
-	# print all pages
+		# print all pages
 		$vars{'PagesList'} = &listpages("select title from pages;"
-	                                        ,"<a href=\"./index.pl?page=%s\">%s</a><br />") if not defined $query{'query'};
+			,"<a href=\"./index.pl?page=%s\">%s</a><br />") if not defined $query{'query'};
 		$htmlhead .= '<title>PagesList@'.$vars{'SiteName'}.'</title>';
 		$htmlbody .= &tmpl2html('html/list.html',\%vars);
 		delete $vars{'PagesList'};
@@ -176,12 +174,12 @@ sub search {
 
 } 
 sub category {
-# print categories
+	# print categories
 	my $query = &security::textalize(&security::exorcism($query{'query'}));
 	$query =~ s/\s/AND/g;
 	$vars{'Query'} = $query{'query'};
 	$vars{'CategoryList'} = &listcategory("select title from pages where tags like '%$query%';"
-	                                      ,"<a href=\"./index.pl?page=%s\">%s</a><br />");
+		,"<a href=\"./index.pl?page=%s\">%s</a><br />");
 	$htmlhead .= '<title>Search &gt; Category@'.$vars{'SiteName'}.'</title>';
 	$htmlbody .= &tmpl2html('html/category.html',\%vars);
 	delete $vars{'CategoryList'};
@@ -189,49 +187,37 @@ sub category {
 
 } 
 sub upload {
-# print upload form
+	# print upload form
 	$htmlhead .= '<title>'.$vars{'PageName'}. ' &gt; Upload@'.$vars{'SiteName'}.'</title>';
 	$htmlbody .= &tmpl2html('html/upload.html',\%vars);
 
 } 
 sub addfile {
-# submit file
+	# submit file
 	my ($title,$modifieddate) = (&fetch2edit)[0,1];
 
 	$htmlhead .= '<title>'.$vars{'PageName'}. ' &gt; UploadProcess@'.$vars{'SiteName'}.'</title>';
 	my $filename = &security::html(&security::exorcism($query{'filename'}));
 	my $original = &security::html(&security::exorcism($query{'orig'}));
-my @res = (&sql::fetch("select confer from pages where title='$vars{'PageName'}';",$data_source));
-	#my @res = (&sql::fetch("select confer from pages where title='$vars{'PageName'}';",$data_source));
-my $files = $res[0];
-if ($files =~ /$filename/) {
+	my @res = (&sql::fetch("select confer from pages where title='$vars{'PageName'}';",$data_source));
+	my $files = $res[0];
+	if ($files =~ /$filename/) {
 
-} else {
-my $tmp  = &date::spridate('%04d %2d %2d %2d:%02d:%02d');
-$files .= "[$filename/$original($tmp)]";
+	} else {
+		my $tmp  = &date::spridate('%04d %2d %2d %2d:%02d:%02d');
+		$files .= "[$filename/$original($tmp)]";
 		&sql::do("update pages set lastmodified_date='$modifieddate', confer='$files' where title='$vars{'PageName'}';"
-	         ,$data_source);
-}
+			,$data_source);
+	}
+	# TODO: これはあくまで暫定処置 いずれ全体的な構造を見直す
+	$htmlbody = "";
+	$sidebar = "";
 }
 #else {
 #}
 
-
-
-
-my $ad = <<__EOD__;
-<script>
-google_ad_client = "pub-7822764449175755";
-/* 336x280, 作成済み 10/02/25 */
-google_ad_slot = "8782365220";
-google_ad_width = 336;
-google_ad_height = 280;
-</script>
-<script src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-__EOD__
-
 $htmlfoot .= "<hr /><address>KeiSpade CMS $VER by Keiya Chinen</address>";
-print '<!DOCTYPE html><head>'.$htmlhead.'</head><body><header>'.$htmlbdhd.'</header><div id="container"><div id="main_container"><section>'.$htmlbody.'</section><hr /><section>'.$ad.'</section></div><aside><dl id="page_menu">'.$sidebar.'</dl></aside></div><footer>'.$htmlfoot.'</footer>';
+print '<!DOCTYPE html><head>'.$htmlhead.'</head><body><header>'.$htmlbdhd.'</header><div id="container"><div id="main_container"><section>'.$htmlbody.'</section><hr /></div><aside><dl id="page_menu">'.$sidebar.'</dl></aside></div><footer>'.$htmlfoot.'</footer>';
 print "</body></html>";
 
 
@@ -254,7 +240,7 @@ sub fetch2edit {
 	}
 	$modified_date = time();
 	$created_date = time();
-	
+
 	chomp ($title,$modified_date,$created_date,$tags,$autotags,$confer,$copyright,$body);
 	return ($title,$modified_date,$created_date,$tags,$autotags,$confer,$copyright,$body);
 }
@@ -277,12 +263,12 @@ sub listcategory {
 	my $format = $_[1];
 	my %category;
 	foreach my $tmp (@res) {
-	my @tags = split(/\|/, $tmp);
+		my @tags = split(/\|/, $tmp);
 		foreach my $tag (@tags) {
 			my $formatmp = $format;
 			$formatmp =~ s/%s/$tag/g;
 			$categorylist .= $formatmp if not exists $category{$tag};
-	$category{$tag} = 1;
+			$category{$tag} = 1;
 		}
 	}
 	return $categorylist;
