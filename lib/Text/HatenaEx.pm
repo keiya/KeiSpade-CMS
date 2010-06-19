0ea90a5bace539708f07f4bca1f68b4209dfec82
Haroperi <haroperitan@gmail.com>
2010-06-19 18:24:57 +0900

    jissou video, audio tag

--- lib/Text/HatenaEx.pm
+++ lib/Text/HatenaEx.pm
@@ -9,16 +9,16 @@ use base qw(Text::Hatena);
 
 __PACKAGE__->syntax(q(
 	block	: h6
-	| h5
-	| h4
-	| blockquote
-	| dl
-	| list
-	| super_pre
-	| pre
-	| table
-	| cdata
-	| p
+		| h5
+		| h4
+		| blockquote
+		| dl
+		| list
+		| super_pre
+		| pre
+		| table
+		| cdata
+		| p
 	h6	: "\n****" inline(s)
 ));
 
@@ -29,4 +29,21 @@ sub h6 {
 	return "<h6>$title</h6>";
 }
 
+sub video {
+	my $mvar = shift;
+	my $url = $mvar->[1];
+	return sprintf( '<video src="%s" controls="controls">%s</video>', $url, $url);
+}
+
+sub audio {
+	my $mvar = shift;
+	my $url = $mvar->[1];
+	return sprintf( '<audio src="%s" controls="controls">%s</audio>', $url, $url);
+}
+
+# AutoLinkを拡張する
+Text::Hatena::AutoLink->syntax({
+	'\[(.*):video\]' => \&Text::HatenaEx::video,
+	'\[(.*):audio\]' => \&Text::HatenaEx::audio,
+});
 
