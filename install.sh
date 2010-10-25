62b8c98f0d301622991f20d5191265bedf427966
keiya <keiya@www4080u.sakura.ne.jp>
2010-10-25 09:15:21 +0900

    trivial fix

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-echo "Installing perl module\n"
-echo "installing DBI::DBD (perl)\n"
+printf "Installing perl module\n"
+printf "installing DBI::DBD (perl)\n"
 perl -MCPAN -e 'install DBI::DBD'
-echo "installing DBD::SQLite (perl)\n"
+printf "installing DBD::SQLite (perl)\n"
 perl -MCPAN -e 'install DBD::SQLite'
 
-echo "\nchanging ./dat permission to 777\n"
+printf "\nchanging ./dat permission to 777\n"
 chmod -R 777 ./dat
 chmod 644 ./dat/.htaccess
-echo "\nchanging ./files permission to 777\n"
+printf "\nchanging ./files permission to 777\n"
 chmod -R 777 ./files
 chmod 644 ./files/.htaccess
 
-echo "\nPlease install sqlite3 if not installed"
+printf "\nPlease install sqlite3 if not installed"
